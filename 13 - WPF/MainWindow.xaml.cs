13fe316a505c426fe1a3d5c57e563805ec17a2ad
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-13 09:22:31 +0100

    ComboBox

--- 13 - WPF/MainWindow.xaml.cs
+++ 13 - WPF/MainWindow.xaml.cs
@@ -24,33 +24,7 @@ namespace _13___WPF
         public MainWindow()
         {
             InitializeComponent();
-            List<Match> matches = new List<Match>();
-            matches.Add(new Match() { Team1 = "Bayern Munich", Team2 = "Real Madrid", Score1 = 3, Score2 = 2, Completion = 85 });
-            matches.Add(new Match() { Team1 = "PSG", Team2 = "Barca", Score1 = 3, Score2 = 2, Completion = 55 });
-            matches.Add(new Match() { Team1 = "BVB Dortmund", Team2 = "AS Roma", Score1 = 0, Score2 = 1, Completion = 25 });
-            matches.Add(new Match() { Team1 = "Man United", Team2 = "La Galaxy", Score1 = 1, Score2 = 1, Completion = 35 });
-            IbMatches.ItemsSource = matches;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (IbMatches.SelectedItem != null)
-            {
-                MessageBox.Show("Selected Match: " + 
-                    (IbMatches.SelectedItem as Match).Team1 + " " +
-                    (IbMatches.SelectedItem as Match).Score1 + " " +
-                    (IbMatches.SelectedItem as Match).Score2 + " " +
-                    (IbMatches.SelectedItem as Match).Team2);
-            }
-        }
-
-        public class Match
-        {
-            public int Score1 { get; set; }
-            public int Score2 { get; set; }
-            public string Team1 { get; set; }
-            public string Team2 { get; set; }
-            public int Completion { get; set; }
+            comboBoxColors.ItemsSource = typeof(Colors).GetProperties();
         }
     }
 }
